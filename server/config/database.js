c49e7d011f5cfe7023281f7930746adfdea65835
Sahil23khanna <156312725+Sahil23khanna@users.noreply.github.com>
2025-05-30 13:27:13 +0530

    change in db

--- server/config/database.js
+++ server/config/database.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://sahilkhanna2330:Ik7H8uZRw79iEBr2@cluster0.fgvi34k.mongodb.net/pathfinders")
-
+mongoose.connect("mongodb+srv://sahilkhanna2330:Ik7H8uZRw79iEBr2@cluster0.fgvi34k.mongodb.net/pathfinders",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    connectTimeoutMS: 50000, 
+    socketTimeoutMS: 60000 
+})
 .then(()=>{
     console.log("Database is connected!!");
 })
-
 .catch((error)=>{
     console.log("Error while Connecting Database ", error);
-    
 })
